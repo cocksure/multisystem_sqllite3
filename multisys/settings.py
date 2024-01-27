aa18cc6cce40cce00d1f15207391d54470450260
cocksure <sanjarwer93@gmail.com>
2024-01-27 10:27:16 +0500

    fix some issue in allowed hosts

--- multisys/settings.py
+++ multisys/settings.py
@@ -84,8 +84,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:8000',
-    'http://http://146.190.60.227:8030',
-
+    'http://146.190.60.227:8030',
 ]
 
 CORS_ALLOW_METHODS = [
